35d038ad2f5f66a79fa46f71b2a51d12bd0fa7df
Abraham <abraham@MacBook-Pro-de-Danae.local>
2021-01-29 21:02:34 -0600

    Update: copy to clipboard

--- index.js
+++ index.js
@@ -2,11 +2,26 @@ var button = document.getElementById('execute');
 var inputFuction = document.getElementById('inputFunction');
 var inputValue = document.getElementById('inputValue');
 var result = document.getElementById('result');
-var inlS = document.getElementById('inline-script');
+var copyButton = document.getElementById('copy');
 
 button.addEventListener('click', execute);
 
 function execute() {
     var cadena = `${inputFuction.value}("` + inputValue.value + `") `;
     result.innerText = eval(cadena);
+}
+
+copyButton.addEventListener('click', copyToClipboard);
+
+function copyToClipboard() {
+    var tempInput = document.createElement("input");
+    tempInput.value = result.innerText;
+
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    tempInput.setSelectionRange(0, 99999);
+    document.execCommand('copy');
+
+    //alert("Copied: " + tempInput.value);
+    document.body.removeChild(tempInput);
 }
